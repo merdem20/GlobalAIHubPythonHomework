c6d046739f25a22d467f5d375911b30ecf03e68b
merdem20 <76492825+merdem20@users.noreply.github.com>
2020-12-25 23:54:14 +0300

    Add files via upload

---
+++ odev2/main.py
@@ -0,0 +1,7 @@
+isim = input("adınızı giriniz\n")
+soyad = input("soyadınızı giriniz\n")
+yas = int(input("yaşınızı giriniz\n"))
+d_tarih = int(input("doğum yılınızı giriniz (sadece yıl)\n"))
+k_liste = [isim,soyad,yas,d_tarih]
+for i in k_liste:
+    print(k_liste)
