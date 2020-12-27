dc688ce5c5c13ac069551f96a0152f88ebd8be4b
merdem20 <76492825+merdem20@users.noreply.github.com>
2020-12-27 20:08:19 +0300

    Add files via upload

--- odev2/main.py
+++ odev2/main.py
@@ -1,7 +1,16 @@
+import time
+
 isim = input("adınızı giriniz\n")
 soyad = input("soyadınızı giriniz\n")
 yas = int(input("yaşınızı giriniz\n"))
 d_tarih = int(input("doğum yılınızı giriniz (sadece yıl)\n"))
 k_liste = [isim,soyad,yas,d_tarih]
 for i in k_liste:
-    print(k_liste)
+    print(i)
+
+time.sleep(1)
+if yas<18:
+    print("sokağa çıkmak tehlikeli")
+
+else :
+    print("sokağa çıkmak güvrnli")
